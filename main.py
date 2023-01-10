4862137655759a6a9e965bf6e51221a24ce4a1f3
Manoj-Kumar13 <manojkumar777.mk53@gmail.com>
2023-01-10 13:55:10 +0530

    implemented functionality to scrape data of multiple pages

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 
-html_text = requests.get('http://books.toscrape.com/catalogue/page-1.html').text
-
-soup = BeautifulSoup(html_text,'lxml')
-books_data = soup.findAll('article', class_='product_pod')
+total_pages = 50
 
 workbook = xlsxwriter.Workbook('sampleData.xlsx')
 worksheet = workbook.add_worksheet()
@@ -14,24 +11,39 @@ worksheet.set_column('B:B',30)
 worksheet.set_column('C:C',20)
 worksheet.set_column('D:D',30)
 
+workbook = xlsxwriter.Workbook('sampleData.xlsx')
+worksheet = workbook.add_worksheet()
+
 titles = ['Title', 'Rating', 'Price', 'Stock']
 
 for cl in range(4):
-    worksheet.write(0,cl,titles[cl])
+    worksheet.write(0, cl, titles[cl])
 
 row = 1
-for book in books_data:
-    col = 0
-    title = book.h3.a['title']
-    rating = book.p['class']
-    price = book.find('div', class_='product_price').p.string
-    stock = book.find('div', class_='product_price').find('p',class_='instock').contents[2].strip()
-    print(stock)
-    price = price.split('Â')
-    worksheet.write(row, col, title)
-    worksheet.write(row, col+1, rating[-1])
-    worksheet.write(row, col+2, price[1])
-    worksheet.write(row, col+3, stock)
-    row+=1
-print("done!!")
+for page in range(total_pages):
+    html_text = requests.get(f'https://books.toscrape.com/catalogue/page-{page+1}.html').text
+
+    soup = BeautifulSoup(html_text,'lxml')
+    books_data = soup.findAll('article', class_='product_pod')
+
+
+
+
+
+
+    for book in books_data:
+        col = 0
+        title = book.h3.a['title']
+        rating = book.p['class']
+        price = book.find('div', class_='product_price').p.string
+        stock = book.find('div', class_='product_price').find('p',class_='instock').contents[2].strip()
+        # print(stock)
+        price = price.split('Â')
+        worksheet.write(row, col, title)
+        worksheet.write(row, col+1, rating[-1])
+        worksheet.write(row, col+2, price[1])
+        worksheet.write(row, col+3, stock)
+        row+=1
+    print(f"page number : {page+1} done")
+
 workbook.close()
