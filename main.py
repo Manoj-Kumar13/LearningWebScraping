eebaf04dbbb0707f74623ce32bc5b398a958fa60
Manoj-Kumar13 <manojkumar777.mk53@gmail.com>
2023-01-10 12:39:12 +0530

    learned how to find specific elements on the web page

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
+import xlsxwriter
 
-html_text = requests.get('https://books.toscrape.com/').text
+html_text = requests.get('http://books.toscrape.com/catalogue/page-1.html').text
 
 soup = BeautifulSoup(html_text,'lxml')
 books_data = soup.findAll('article', class_='product_pod')
 
+workbook = xlsxwriter.Workbook('sampleData.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.set_column('A:A',80)
+worksheet.set_column('B:B',30)
+worksheet.set_column('C:C',20)
+worksheet.set_column('D:D',30)
+
+titles = ['Title', 'Rating', 'Price', 'Stock']
+
+for cl in range(4):
+    worksheet.write(0,cl,titles[cl])
+
+row = 1
 for book in books_data:
-    print(book.h3.a['title'])
+    col = 0
+    title = book.h3.a['title']
+    rating = book.p['class']
+    price = book.find('div', class_='product_price').p.string
+    stock = book.find('div', class_='product_price').find('p',class_='instock').contents[2].strip()
+    print(stock)
+    price = price.split('Â')
+    worksheet.write(row, col, title)
+    worksheet.write(row, col+1, rating[-1])
+    worksheet.write(row, col+2, price[1])
+    worksheet.write(row, col+3, stock)
+    row+=1
+print("done!!")
+workbook.close()
