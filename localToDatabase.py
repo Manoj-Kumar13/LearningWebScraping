ea5494da22c025fac4d476b95f60f284d3a4a586
Manoj-Kumar13 <manojkumar777.mk53@gmail.com>
2023-01-11 23:26:54 +0530

    local file to database

--- localToDatabase.py
+++ localToDatabase.py
@@ -1,31 +1,24 @@
 from bs4 import BeautifulSoup
-import requests
-import pymongo
 from pymongo import MongoClient
 
 # connecting with mongodb
 cluster = MongoClient('your connection string')
 # giving cluster name
-db = cluster["booksData"]
+db = cluster["localBookData"]
 # giving collection name
 collection = db["allBooks"]
 
-# creating the post data
-# post1 = {"_id": 0, "name": "Chris", "score": 5}
-# post2 = {"_id": 1, "name": "Brad", "score": 7}
-#
-# collection.insert_one(post1)
-# collection.insert_one(post2)
-
-total_pages = 50
+total_pages = 5
 
 titles = ['Title', 'Rating', 'Price', 'Stock']
 
 row = 1
 for page in range(total_pages):
-    html_text = requests.get(f'https://books.toscrape.com/catalogue/page-{page+1}.html').text
+    #reading local file
+    with open(f'./sampleWebpages/page{page+1}.html', 'r') as html_file:
+        content = html_file.read()
 
-    soup = BeautifulSoup(html_text,'lxml')
+    soup = BeautifulSoup(content,'lxml')
     books_data = soup.findAll('article', class_='product_pod')
 
     for book in books_data:
@@ -35,6 +28,8 @@ for page in range(total_pages):
         price = book.find('div', class_='product_price').p.string
         stock = book.find('div', class_='product_price').find('p',class_='instock').contents[2].strip()
         price = price.split('Â')
+
+        # print(f'title = {title}, rating = {rating}, price = {price}, stock = {stock}\n')
 
         post = {"_id": row, "Title": title, "Rating": rating[-1], "price": price[1], "Stock": stock}
 
