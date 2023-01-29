fc175a8714a66146c6858d95050923c3b0fdff83
Manoj-Kumar13 <manojkumar777.mk53@gmail.com>
2023-01-29 22:11:21 +0530

    built a password generator

--- learningPython.py
+++ learningPython.py
@@ -233,3 +233,53 @@ print(round(2.545454,2))
 # round number to certain decimal places using formatting
 bill_amount = 5
 print("{:.2f}".format(bill_amount))
+
+# min max function in python
+arr = [2,5,8,2,47,9,1]
+
+print(f"max in arr {max(arr)}")
+print(f"min im arr {min(arr)}")
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+letters_len = len(letters)
+numbers_len = len(numbers)
+symbols_len = len(symbols)
+
+letters_gen = ""
+
+for x in range(nr_letters-nr_symbols-nr_numbers):
+  letter_index = random.randint(0,letters_len-1)
+  letters_gen += letters[letter_index]
+
+symbols_gen = ""
+for x in range(nr_symbols):
+  symbol_index = random.randint(0,symbols_len-1)
+  symbols_gen += symbols[symbol_index]
+
+number_gen = ""
+for x in range(nr_numbers):
+  number_index = random.randint(0,numbers_len-1)
+  number_gen += numbers[number_index]
+
+password = letters_gen + symbols_gen + number_gen
+print("Easy password",password)
+
+
+
+#Hard Level - Order of characters randomised:
+temp_list = list(password)
+random.shuffle(temp_list)
+print("Hard password",''.join(temp_list))
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
